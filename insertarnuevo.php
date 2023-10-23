a4682926daa80030c16b69feda17ecb6165ac571
Tomas Garza Moya <tomasgarzamoya@Tomass-MacBook-Air.local>
2023-10-23 14:33:43 -0500

    Practica de pokemon

--- insertarnuevo.php
+++ insertarnuevo.php
@@ -1,24 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style1.css">
-    <title>Insertar</title>
-</head>
-<body>
-    <h1>Insertar Valores</h1>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" id="formulario">
-        <label for="nombre">Nombre: </label>
-        <input type="text" id="nombre" name="nombre" required><br>
-        <label for="apellido">Apellido: </label>
-        <input type="text" id="apelldio" name="apellido" required><br>
-        <label for="nacimiento">Nacimiento: </label>
-        <input type="text" id="nacimiento" name="nacimiento" required><br>  
-        <input type="submit" value="Agregar registro"> 
-    </form>
-    <?php
+<?php
      // Configuración de la conexión a la base de datos
      $servername = "localhost";
      $username = "root";
@@ -49,5 +29,3 @@
     }
 }
     ?>
-</body>
-</html>
--- mostrar.php
+++ mostrar.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style1.css">
+    <title>Mostrar</title>
 </head>
 <body>
     <h1>DATOS DE LA TABLA</h1>
@@ -23,9 +24,9 @@
     $result = $conn->query($sql);
     if($result->num_rows >0){
         echo "<table>";
-        echo "<tr><th>id</th><th>nombre</th><th>apellido</th><th>nacimiento</th></tr>";
+        echo "<tr><th>id</th><th>Nombre</th><th>Apellido</th><th>Nacimiento</th></tr>";
         while($row = $result->fetch_assoc()){
-            echo "<tr><td>" . $row["id"] . "</td><td>" . $row["nombre"] . "</td><td>" . $row["apellido"] . "</td><td>" . $row["nacimiento"] . "</td><td>";
+            echo "<tr><td>" . $row["id"] . "</td><td>" . $row["nombre"] . "</td><td>" . $row["apellido"] . "</td><td>" . $row["nacimiento"] . "</td></tr>";
         }
         echo "</table>";
     } else {
